1da05eb9419eafc72909683004efb56d05b83407
Adrian Cole <adrian@tetrate.io>
2022-05-26 18:11:01 -0700

    Adds some clarifying documentation
    Probably, compilation instructions should be added to the README,
    but I'm not sure how to install Javy.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	_ "embed"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/tetratelabs/wazero"
@@ -24,30 +24,35 @@ func main() {
 	r := wazero.NewRuntime()
 	defer r.Close(ctx) // This closes everything this Runtime created.
 
-	config := wazero.NewModuleConfig().WithStdout(os.Stdout)
-
 	// Instantiate WASI, which implements system I/O such as console output.
 	if _, err := wasi.InstantiateSnapshotPreview1(ctx, r); err != nil {
 		log.Panicln(err)
 	}
 
+	// Compile the module to reduce performance impact of instantiating it multiple times.
 	compiled, err := r.CompileModule(ctx, greetWasm, wazero.NewCompileConfig())
-
 	if err != nil {
 		log.Panicln(err)
 	}
 
+	config := wazero.NewModuleConfig().WithStdout(os.Stdout)
 	for i := 0; i < 100; i++ {
 		start := time.Now()
-		input := bytes.NewBufferString(fmt.Sprintf(`{"name": "Person %d"}`, i))
 
-		module, err := r.InstantiateModule(ctx, compiled, config.WithName(fmt.Sprintf("module-%d", i)).WithStdin(input))
+		// The only way to pass data to Javy is via Stdin, arguments or env variables.
+		input := strings.NewReader(fmt.Sprintf(`{"name": "Person %d"}`, i))
+		startModule(ctx, r, compiled, config.WithStdin(input))
 
-		if err != nil {
-			log.Panicln(err)
-		}
-
-		module.Close(nil)
 		fmt.Println("time taken", time.Since(start))
 	}
+}
+
+// startModule implicitly invokes the "_start" function defined by Javy.
+// Note: If you want to run modules in parallel, config must override the name.
+func startModule(ctx context.Context, r wazero.Runtime, compiled wazero.CompiledModule, config wazero.ModuleConfig) {
+	module, err := r.InstantiateModule(ctx, compiled, config)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer module.Close(ctx)
 }
