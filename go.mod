c52149d8dc052b1002be5d3b2fc2a58a9a1b0c50
Adrian Cole <adrian@tetrate.io>
2022-08-30 13:22:41 +0800

    Updates to wazero beta
    This updates to the first beta release of [wazero](https://wazero.io), 1.0.0-beta.1.
    
    Future betas will release at the end of each month until 1.0 in February 2023.
    
    Note: [Release notes](https://github.com/tetratelabs/wazero/releases) will be posted in the next day or two.
    
    Meanwhile, we've also opened a [gophers slack wazero channel](https://gophers.slack.com/archives/C040AKTNTE0) for support, updates and conversation!
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/F21/javy-wazero
 
 go 1.18
 
-require github.com/tetratelabs/wazero v0.0.0-20220630052417-63f6b2231142
+require github.com/tetratelabs/wazero v1.0.0-beta.1
--- suborbital-javy/suborbital_test.go
+++ suborbital-javy/suborbital_test.go
@@ -16,7 +16,7 @@ func BenchmarkSuborbitalCallFunction(b *testing.B) {
 	ctx := context.Background()
 
 	// Create a new WebAssembly Runtime.
-	r := wazero.NewRuntime()
+	r := wazero.NewRuntime(ctx)
 	defer r.Close(ctx) // This closes everything this Runtime created.
 
 	// Instantiate WASI, which implements system I/O such as console output.
